2cf0e543520aa581a9f8781656e71868b6622ff1
JimJarvis <jimfanspire@gmail.com>
2014-11-09 10:40:39 -0500

    Starter code

---
+++ environ.sh
@@ -0,0 +1,5 @@
+export PATH=/usr/lib/jvm/java-1.7.0-openjdk-amd64/bin:$PATH
+export PYTHONPATH=/home/cs4701/python/lib/python2.7/site-packages/
+export APP_HOME=/home/cs4701/Project3/source_code/derek
+export LIB=$APP_HOME/lib
+export CP=$LIB/lucene-core-4.6.0.jar:$LIB/lucene-analyzers-common-4.6.0.jar:$LIB/lucene-queryparser-4.6.0.jar:$LIB/json.jar:$APP_HOME/classes/
